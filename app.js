04161aef871b895f2d618508c1d8702200e75e1e
shaikh-riyaz-ali <sshaikhriyazali@gmail.com>
2025-03-17 23:24:16 +0530

    duhdaduah

---
+++ app.js
@@ -0,0 +1,125 @@
+// exports.add = function(a, b){
+//     return a+b;
+// }
+
+
+// function addition(a, b){
+//     return a+b
+
+// }
+// function substraction(a, b){
+//     return a-b
+
+// }
+// function multiplication(a, b){
+//     return a*b
+
+// }
+
+// function division(a, b){
+//     return a/b
+
+// }
+
+
+// module.exports.addition = addition
+// module.exports.substraction = substraction
+// module.exports.multiplication = multiplication
+// module.exports.division = division
+
+
+
+
+
+
+
+// function addition(a, b){
+//     return a+b
+
+// }
+// function substraction(a, b){
+//     return a-b
+
+// }
+// function multiplication(a, b){
+//     return a*b
+
+// }
+
+// function division(a, b){
+//     return a/b
+
+// }
+
+// module.exports = {addition, substraction, multiplication, division}
+
+
+
+
+
+
+// function framework(...framework){
+//     return framework
+
+// }
+// function lib(...lib){
+//     return lib
+
+// }
+// module.exports = {framework, lib}
+
+
+
+
+// async function fetchapi(){
+//     let data = await fetch('https://dummyjson.com/products')
+//     console.log(data);
+//     let result = await data.json()
+//     console.log(result.products);
+//     result.products.map((m)=>{
+//         console.log(m);
+        
+
+//     })
+    
+    
+  
+ 
+// }
+
+// module.exports.fetchapi = fetchapi
+
+
+
+
+// let os = require("os")
+// console.log(os);
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.hostname());
+// console.log(os.machine());
+// console.log(os.platform());
+// console.log(os.version());
+// console.log(os.userInfo());
+// console.log(os.type());
+
+// let path = require('path')
+// console.log(path.basename("c/users/admin/website/index.html"))
+
+// let path = require('path'); 
+// console.log(path.dirname("c/users/admin/website/index.html"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
