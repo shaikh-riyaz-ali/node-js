73f0cc1b8126aa5b55c4835c7ed309310029f4c7
shaikh-riyaz-ali <sshaikhriyazali@gmail.com>
2025-03-18 10:48:50 +0530

    added new methods

--- app.js
+++ app.js
@@ -1,113 +1,113 @@
-// exports.add = function(a, b){
-//     return a+b;
-// }
+exports.add = function(a, b){
+    return a+b;
+}
 
 
-// function addition(a, b){
-//     return a+b
+function addition(a, b){
+    return a+b
 
-// }
-// function substraction(a, b){
-//     return a-b
+}
+function substraction(a, b){
+    return a-b
 
-// }
-// function multiplication(a, b){
-//     return a*b
+}
+function multiplication(a, b){
+    return a*b
 
-// }
+}
 
-// function division(a, b){
-//     return a/b
+function division(a, b){
+    return a/b
 
-// }
-
-
-// module.exports.addition = addition
-// module.exports.substraction = substraction
-// module.exports.multiplication = multiplication
-// module.exports.division = division
+}
 
 
-
-
-
-
-
-// function addition(a, b){
-//     return a+b
-
-// }
-// function substraction(a, b){
-//     return a-b
-
-// }
-// function multiplication(a, b){
-//     return a*b
-
-// }
-
-// function division(a, b){
-//     return a/b
-
-// }
-
-// module.exports = {addition, substraction, multiplication, division}
+module.exports.addition = addition
+module.exports.substraction = substraction
+module.exports.multiplication = multiplication
+module.exports.division = division
 
 
 
 
 
 
-// function framework(...framework){
-//     return framework
 
-// }
-// function lib(...lib){
-//     return lib
+function addition(a, b){
+    return a+b
 
-// }
-// module.exports = {framework, lib}
+}
+function substraction(a, b){
+    return a-b
+
+}
+function multiplication(a, b){
+    return a*b
+
+}
+
+function division(a, b){
+    return a/b
+
+}
+
+module.exports = {addition, substraction, multiplication, division}
 
 
 
 
-// async function fetchapi(){
-//     let data = await fetch('https://dummyjson.com/products')
-//     console.log(data);
-//     let result = await data.json()
-//     console.log(result.products);
-//     result.products.map((m)=>{
-//         console.log(m);
+
+
+function framework(...framework){
+    return framework
+
+}
+function lib(...lib){
+    return lib
+
+}
+module.exports = {framework, lib}
+
+
+
+
+async function fetchapi(){
+    let data = await fetch('https://dummyjson.com/products')
+    console.log(data);
+    let result = await data.json()
+    console.log(result.products);
+    result.products.map((m)=>{
+        console.log(m);
         
 
-//     })
+    })
     
     
   
  
-// }
+}
 
-// module.exports.fetchapi = fetchapi
-
-
+module.exports.fetchapi = fetchapi
 
 
-// let os = require("os")
-// console.log(os);
-// console.log(os.arch());
-// console.log(os.cpus());
-// console.log(os.hostname());
-// console.log(os.machine());
-// console.log(os.platform());
-// console.log(os.version());
-// console.log(os.userInfo());
-// console.log(os.type());
 
-// let path = require('path')
-// console.log(path.basename("c/users/admin/website/index.html"))
 
-let path = require('path'); 
-console.log(path.dirname("c/users/admin/website/index.html"))
+let os = require("os")
+console.log(os);
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.hostname());
+console.log(os.machine());
+console.log(os.platform());
+console.log(os.version());
+console.log(os.userInfo());
+console.log(os.type());
+
+let path1 = require('path')
+console.log(path1.basename("c/users/admin/website/index.html"))
+
+let path2 = require('path'); 
+console.log(path2.dirname("c/users/admin/website/index.html"))
 
 
 
