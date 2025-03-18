ca76963be546087664d96bdf930aa3e68f68a88a
shaikh-riyaz-ali <sshaikhriyazali@gmail.com>
2025-03-18 10:58:49 +0530

    add new method

--- add.js
+++ add.js
@@ -1,37 +1,35 @@
 
-// let fun = require("./app")
-// console.log(fun.add(10, 20));
+let fun = require("./app")
+console.log(fun.add(10, 20));
 
 
 
-
-
-// let fun = require("./app")
-// console.log(fun.addition(10, 20));
-// console.log(fun.substraction(10, 20));
-// console.log(fun.multiplication(10, 20));
-// console.log(fun.division(10, 20));
-
-
-
-// let {addition, substraction, multiplication, division} = require("./app")
-// console.log(addition(10, 20));
-// console.log(substraction(10, 20));
-// console.log(multiplication(10, 20));
-// console.log(division(10, 20));
-
-
-// let {framework, lib} = require("./app")
-// console.log(framework("reactjs", "angular"));
-// console.log(lib("nodejs", "libu"))
-
-
-// let fetch = require("./app")
-// console.log(fetch.fetchapi());
 
 
 let fun = require("./app")
-console.log(fun.add());
+console.log(fun.addition(10, 20));
+console.log(fun.substraction(10, 20));
+console.log(fun.multiplication(10, 20));
+console.log(fun.division(10, 20));
+
+
+
+let {addition, substraction, multiplication, division} = require("./app")
+console.log(addition(10, 20));
+console.log(substraction(10, 20));
+console.log(multiplication(10, 20));
+console.log(division(10, 20));
+
+
+let {framework, lib} = require("./app")
+console.log(framework("reactjs", "angular"));
+console.log(lib("nodejs", "libu"))
+
+
+let fetch = require("./app")
+console.log(fetch.fetchapi());
+
+
 
 
 
